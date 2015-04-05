67bdf95725ad73e842afdedd5664d95337fb1675
Cestaberous <cestaberous@mail.com>
2015-04-05 22:30:21 +0000

    automated publish from C9

---
+++ depPage/header/header.js
@@ -0,0 +1,23 @@
+document.write('<head>\
+    <link rel="stylesheet" type="text/css" href="/theme.css"/>\
+    <script language="javascript" type="text/javascript" src="/jquery-1.11.2.min.js"></script>\
+    <script language="javascript" type="text/javascript" src="/header.js"></script>\
+    <title>Home | Cestaberous</title>\
+  </head>\
+  <header>\
+  <div class="header-banner">\
+    <a href="/" class="logo"></a>\
+    <h1>Cestaberous</h1>\
+  </div>\
+\
+  <nav>\
+    <ul>\
+      <li><a href="/mods">Mods</a></li>\
+      <li><a href="/serverinfo">Server Information</a></li>\
+      <li><a href="/contact">Contact</a></li>\
+      <li><a href="/links">Links</a></li>\
+      <li><a href="http://github.com/cestaberous">GitHub</a></li>\
+      \
+    </ul>\
+  </nav>\
+</header>');
