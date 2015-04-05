0199c841ef0fb46a1a4e1c0b56aa5c7fa7cbb285
Cestaberous <cestaberous@mail.com>
2015-04-05 03:22:49 +0000

    automated publish from C9

---
+++ header.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
